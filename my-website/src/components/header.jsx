d9a4959b130e40cca1bc7f33bf9a3cf0de0b8b97
Srikant Vasudevan <sv092003@gmail.com>
2022-01-04 21:57:53 -0500

    functional header page

--- my-website/src/components/header.jsx
+++ my-website/src/components/header.jsx
@@ -3,12 +3,19 @@ import { AccountCircle } from '@mui/icons-material';
 import React, { Component } from 'react';
 import Navigation from './nav';
 import Footer from './footer';
+import TextLoop from "react-text-loop";
+import logo from './profile.jpg';
+
 
 
 class Header extends React.Component {
     render() { 
         const learnMoreStyle = {
             borderRadius: '50px'
+        };
+        const imgStyle = {
+            border: '5px solid #1976d2',
+            borderRadius: '50%',
         }
         return (
         <div>
@@ -16,13 +23,20 @@ class Header extends React.Component {
             <Grid container spacing={10}>
                 <Grid item md={6}>
                     <Container style={{
-                        paddingLeft: '250px',
+                        paddingLeft: '200px',
                         paddingRight: '0px',
-                        paddingTop: '150px'
+                        paddingTop: '100px'
                     }}>
-                        <h1 style={{fontSize: '48px'}}>I'm Srikant!</h1>
-                        <h1 style={{fontSize: '24px'}}>Student. Programmer. Enthusiast.</h1>
-                        <p style={{fontSize: '18px'}}>An enthusiastic computer science major at Georgia Tech who loves to solve problems, learn new things, and of course, build cool stuff.</p>
+                        <h1 style={{fontSize: '58px'}}>My name is Srikant!</h1>
+                        <h1 style={{fontSize: '32px'}}>
+                            I'm a {" "}
+                        <TextLoop>
+                            <span>student.</span>
+                            <span>programmer.</span>
+                            <span>creator.</span>
+                        </TextLoop>
+                        </h1>
+                        <p style={{fontSize: '24px'}}>An enthusiastic computer science major at Georgia Tech who loves to solve problems, learn new things, and of course, build cool stuff.</p>
                         <Button style={learnMoreStyle} variant="contained">Learn More</Button>
                     </Container>
                 </Grid>
@@ -30,7 +44,7 @@ class Header extends React.Component {
                     <Container style={{
                         padding: '150px'
                     }}>
-                        <h4>Something Else</h4>
+                        <img style={imgStyle} width={300} src={logo} alt="Logo" />;
                     </Container>
                 </Grid>
             </Grid>
